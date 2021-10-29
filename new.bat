bf6c413a0fb187f7fc0ad817e24b726fb7187f83
DorkYBru <76796721+DorkYBru@users.noreply.github.com>
2021-10-29 21:54:18 +0200

    MMMH

---
+++ new.bat
@@ -0,0 +1,17 @@
+@echo off
+color 2
+title TheScrpt
+echo "Minecraft Jest OP"
+:ez
+
+
+
+
+python scan.py
+
+set /A tit=%tit% + 1
+title %tit%
+ 
+echo Scan %tit%
+
+goto ez
---
+++ scan.py
@@ -0,0 +1,32 @@
+import random
+import sys 
+import requests
+from discord_webhook import DiscordWebhook
+from bs4 import BeautifulSoup
+
+targeturl = 'https://flipbook.apps.gwo.pl/display/'
+
+num = random.randint(0,100000)
+nnum = (num < 100000)
+url = 'https://flipbook.apps.gwo.pl/display/{}'  .format(random.randint(0,60000))
+
+
+reqs = requests.get(url)
+  
+
+
+soup = BeautifulSoup(reqs.text, 'html.parser')
+  
+gwo = "Gdańskie Wydawnictwo Oświatowe"
+
+for title in soup.find_all('title'):
+    webhk = title.get_text()
+    print(title)
+    if gwo in webhk:
+        quit()
+    stdoutOrigin=sys.stdout
+    webhook = DiscordWebhook(url='https://discordapp.com/api/webhooks/903722178678239233/CaJj2UuGrdOQwq23I1BKYDTivM48iu8955LnIThITzA1sYABFDnekbMXyIJv2r7pkF48', content = webhk + "\n" + "> " + url)
+    response = webhook.execute()
+
+
+
